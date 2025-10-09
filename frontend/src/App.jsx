36f3685696b9f57242cb6fab8d276114a62cd012
Alex Diniz <alexdinizjr13@gmail.com>
2025-10-09 00:14:43 -0300

    ajustes para deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ScrollToTop from "./components/layout/ScrolltoTop";
+import ScrollToTop from "./components/layout/ScrollToTop";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 
